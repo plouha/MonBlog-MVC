6fea773cbb05218626b4bcb16099e63337026f74
Bernard Germain <plouha57@gmail.com>
2018-07-04 14:05:10 +0200

    Correction5 du code

---
+++ Controleur/Controleur/ControleurCommentaire.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Bernard Germain
+ * Date: 28/05/2018
+ */
+
+require_once 'Controleur/ControleurBillet.php';
+require_once 'Modele/Commentaire.php';
+require_once 'Modele/Billet.php';
+require_once 'Vue/Vue.php';
+
+class ControleurCommentaire {
+
+
+  private $commentaire;
+
+  public function __construct() {
+    $this->commentaire = new Commentaire();
+    $this->billet = new Billet();
+  }
+
+  //affiche le formulaire de moderation d'un commentaire
+  public function vue($idCom) {
+      session_start();
+
+      $commentaire = $this->commentaire->getCommentaire($idCom);
+
+      $vue = new Vue("ModifierCom");
+      $vue->generer(array('commentaire' => $commentaire)); 
+  }
+
+
+  // Affiche la liste de tous les commentaires du blog
+  public function blog2() {
+        session_start();
+        $blog2 = $this->commentaire->getBlog2();
+        $vue = new Vue("Blog2");
+        $vue->generer(array('blog2' => $blog2));
+  }
+
+  
+  //Confirme la modification d'un commentaire
+  public function moderer($idCom, $val, $contenu) {
+       session_start();
+      $this->commentaire->moder($idCom, $val, $contenu);
+      $vue = new Vue("Admin"); // on revient au menu administrateur
+      $vue->generer(array(NULL));
+  }
+  
+  //affiche la page de confirmation de suppression d'un commentaire
+  public function vueConfirmation2($idCom) {
+      session_start();
+      $commentaire = $this->commentaire->getCommentaire($idCom);
+
+      $vue = new Vue("Confirmation2");
+      $vue->generer(array('commentaire' => $commentaire));
+  }
+  
+  //confirme la suppression d'un commentaire
+  public function confirmer2($idCom) {
+      $this->commentaire->confirmer2($idCom);
+      $this->blog2();
+
+  }
+}"\r\n";
